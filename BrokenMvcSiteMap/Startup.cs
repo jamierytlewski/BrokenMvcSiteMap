51556002b1b8943178caec9f7b48f74ef9e8cea9
Jamie R. Rytlewski <jamier.net@gmail.com>
2014-03-28 12:07:57 -0400

    Initial Commit

---
+++ BrokenMvcSiteMap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrokenMvcSiteMap.Startup))]
+namespace BrokenMvcSiteMap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
